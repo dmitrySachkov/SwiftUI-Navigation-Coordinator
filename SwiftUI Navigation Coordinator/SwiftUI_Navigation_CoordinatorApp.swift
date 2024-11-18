f62d463262cbf56fe8f646640b93663a999a6944
domont <d.a.sachkov@gmail.com>
2024-11-18 13:31:17 +0300

    Initial Commit

---
+++ SwiftUI Navigation Coordinator/SwiftUI_Navigation_CoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Navigation_CoordinatorApp.swift
+//  SwiftUI Navigation Coordinator
+//
+//  Created by Dmitry Sachkov on 18.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Navigation_CoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI Navigation CoordinatorTests/SwiftUI_Navigation_CoordinatorTests.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftUI_Navigation_CoordinatorTests.swift
+//  SwiftUI Navigation CoordinatorTests
+//
+//  Created by Dmitry Sachkov on 18.11.2024.
+//
+
+import Testing
+
+struct SwiftUI_Navigation_CoordinatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
